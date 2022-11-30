e4fb8fc441bdf26405d0eb60f8f9b46a05278db7
Nma Shawkat <nmashawket@gmail.com>
2022-11-30 17:42:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Code your solution in this file!
+const hqLocation = 42
+let startingLocation;
+let endingLocation;
+let distance;
+let distanceInFeet;
+let distanceBetweenLocations;
+let distanceBetweenLocationsInFeet;
+let farePrice;
+
+
+function distanceFromHqInBlocks(startingLocation){
+    if (startingLocation >= 43){
+        distance = (startingLocation - hqLocation)
+        return distance
+    }
+    else if (startingLocation <= 41){
+        distance = (hqLocation - startingLocation)
+        return distance
+    }
+
+}
+
+function distanceFromHqInFeet(startingLocation){
+    distanceFromHqInBlocks(startingLocation);
+    distanceInFeet = (distance * 264)
+    return distanceInFeet
+
+}
+
+function distanceTravelledInFeet(startingLocation, endingLocation){
+    if (startingLocation > endingLocation){
+        distanceBetweenLocations = (startingLocation - endingLocation)
+        distanceBetweenLocationsInFeet = (distanceBetweenLocations * 264)
+        return distanceBetweenLocationsInFeet
+    }
+    else if (startingLocation < endingLocation){
+        distanceBetweenLocations = (endingLocation - startingLocation)
+        distanceBetweenLocationsInFeet = (distanceBetweenLocations * 264)
+        return distanceBetweenLocationsInFeet
+
+    }
+}
+
+function calculatesFarePrice(startingLocation, endingLocation){
+    distanceTravelledInFeet(startingLocation, endingLocation);
+    if (distanceBetweenLocationsInFeet <= 400){
+        farePrice = 0
+        return farePrice;
+    }
+    else if (distanceBetweenLocationsInFeet <= 2000){
+        farePrice = ((distanceBetweenLocationsInFeet - 400) * 2)
+        farePrice = farePrice / 100
+        return farePrice;
+    }
+    else if (distanceBetweenLocationsInFeet <= 2500){
+        farePrice = 25
+        return farePrice;
+    }
+    else if (distanceBetweenLocationsInFeet >= 2501){
+        farePrice = 'cannot travel that far'
+        return farePrice;
+    }
+}
